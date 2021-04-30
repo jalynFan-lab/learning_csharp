f1c3fb42bbae9bba7a019d680b0772713ffe2151
jalynFan-lab <70643835+jalynFan-lab@users.noreply.github.com>
2021-04-30 12:30:23 +0900

    Add files via upload

---
+++ virtual練習/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace virtual練習
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //小孩 老人 年轻人
+
+            Person pr = new Person();
+
+            Chidren chidren = new Chidren();
+
+            Old old = new Old();
+
+            Young young = new Young();
+
+            Person[] pers = { pr,chidren,old,young};
+
+            foreach (var item in pers)
+            {
+                item.Walk();
+            }
+
+        }
+    }
+
+    class Person
+    {
+        public virtual void Walk()
+        {
+            Console.WriteLine("人間走");
+        }
+    }
+
+    class Chidren:Person
+    {
+        public override void Walk() 
+        {
+            Console.WriteLine("小孩爬着走");
+        }
+    }
+
+    class Old : Person
+    {
+        public override void Walk()
+        {
+            Console.WriteLine("老人拄拐走");
+        }
+    }
+
+    class Young : Person
+    {
+        public override void Walk()
+        {
+            Console.WriteLine("年轻人两腿走");
+        }
+    }
+
+}
---
+++ 事件与委托/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace 事件与委托
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyEvent my = new MyEvent();
+            //实例化事件，将委托与方法绑定后指向事件
+            my.BuyEvent += new MyEvent.BuyDelegate(my.BuyBanana);
+            //触发事件
+            my.BuyTrigger();
+
+            Action action = my.BuyBanana;
+
+            action();   
+                
+                }
+    }
+
+    class MyEvent
+    {
+        //定义委托
+        public delegate void BuyDelegate();
+        //定义事件
+        public event BuyDelegate BuyEvent;
+        //定义委托中使用的方法
+        public void BuyBanana()
+        {
+            Console.WriteLine("banana");
+        }
+        public void BuyApple()
+        {
+            Console.WriteLine("apple");
+        }
+        public void BuyOrange()
+        {
+            Console.WriteLine("orange");
+        }
+        //定义事件触发器
+        public void BuyTrigger()
+        {
+            BuyEvent();
+        }
+    }
+}
